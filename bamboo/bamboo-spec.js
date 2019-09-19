cd423c05ef72b40d53123de4aecb760ffea8da7b
cambiph <incubusattax@gmail.com>
2019-09-19 14:28:53 +0200

    Merge specs with permissions

--- bamboo/bamboo-spec.js
+++ bamboo/bamboo-spec.js
@@ -1,9 +1,13 @@
 const yaml = require('yaml');
 const utils = require('./utils/util');
+const Document = require('yaml')(Document);
+const YamlSeq = require('yaml')(YamlSeq);
 
-const planProjectKey = 'WEBCOM';
+const planProjectKey = 'WEBCO';
 const packageName = utils.getPackageName();
+const packageName = 'vl-ui-checkbox';
 const planKey = utils.getPlanKey();
+const planKey = 'ABC';
 const planName = 'webcomponent-' + packageName;
 
 const spec = utils.getSpec();
@@ -16,5 +20,12 @@ plan.set('name', planName);
 
 permissions.get('plan').set('key', planKey);
 
+const specNode = yaml.createNode(spec);
+const permissionNode = yaml.createNode(permissions);
+const doc = new Document();
+doc.contents = new YamlSeq();
+doc.contents.items = [ specNode, permissionNode ]
+
+utils.writeYaml(yaml.stringify(doc), '../../bamboo-specs/out.yml');
 utils.writeYaml(yaml.stringify(spec), '../../bamboo-specs/bamboo.yml');
 utils.writeYaml(yaml.stringify(permissions), '../../bamboo-specs/permissions.yml');
