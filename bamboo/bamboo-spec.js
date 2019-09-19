a913c52af78523fed0b778ae60eb94d94da9a3ff
cambiph <incubusattax@gmail.com>
2019-09-19 14:44:45 +0200

    Bugfix

--- bamboo/bamboo-spec.js
+++ bamboo/bamboo-spec.js
@@ -1,6 +1,9 @@
 const yaml = require('yaml');
 const utils = require('./utils/util');
 
+const YamlSeq = require('yaml/types')('YamlSeq');
+const Docu = require('yaml/types')('Document');
+
 const planProjectKey = 'WEBCO';
 const packageName = utils.getPackageName();
 const planKey = utils.getPlanKey();
@@ -18,8 +21,8 @@ permissions.get('plan').set('key', planKey);
 
 const specNode = yaml.createNode(spec);
 const permissionNode = yaml.createNode(permissions);
-const doc = new yaml.Document();
-doc.contents = new yaml.YamlSeq();
+const doc = new Docu();
+doc.contents = new YamlSeq();
 doc.contents.items = [ specNode, permissionNode ]
 
 utils.writeYaml(yaml.stringify(doc), '../../bamboo-specs/out.yml');
