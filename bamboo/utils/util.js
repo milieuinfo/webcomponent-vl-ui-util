01ed614f6d9d7dcc3644b69c5cd792e5606763ab
cambiph <incubusattax@gmail.com>
2019-09-19 12:46:31 +0200

    Permissions genereren via YAML

---
+++ bamboo/utils/util.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const yaml = require('yaml');
+const package = require('../../../package.json');
+
+function writeYaml(input, output) {
+    fs.writeFileSync(output, input, (err) => {
+        if (err) throw err;
+        console.log('YAML has been saved to ' + output);
+    });
+};
+
+function getPackageName() {
+    return JSON.parse(package).name
+}
+
+function getPlanKey() {
+    return getPackageName().split('-').join('').toLocaleUpperCase();
+}
+
+function parseTemplate(fileName) {
+    const file = fs.readFileSync(fileName, 'utf8', (err) => {
+        if (err) throw err;
+        console.debug('Parsing ' + fileName + ' ...');
+    });
+    return yaml.parseDocument(file);
+}
+
+function getSpec() {
+    return parseTemplate('./templates/spec-template.yml').contents;
+}
+
+function getPermissions() {
+    return parseTemplate('./templates/permissions-template.yml').contents;
+}
+
+module.exports = { writeYaml, getPackageName, getPlanKey, getSpec, getPermissions }
