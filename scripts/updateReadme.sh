5dbe464849999e24f7b0d37ad59aab17d1034730
cambiph <incubusattax@gmail.com>
2019-10-30 09:09:19 +0100

    Add debug logging

--- scripts/updateReadme.sh
+++ scripts/updateReadme.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#################################################################################################
+#                                       Helper functions                                        #
+#################################################################################################
+
 CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 installJqDependency() {
@@ -12,8 +16,11 @@ installJqDependency() {
 
 getApiName() {
     IFS='-' # hyphen (-) is set as delimiter
-        read -ra ADDR <<< "$1" # str is read into an array as tokens separated by IFS
+        read -ra ADDR <<< "${1}" # str is read into an array as tokens separated by IFS
+        echo ${1}
+        echo ${ADDR}
         for i in "${ADDR[@]}"; do # access each element of array
+            echo ${apiName}
             apiName+=${i^} #set first letter to uppercase
         done
     IFS=' '
@@ -30,16 +37,26 @@ copyTemplate() {
     cp ${CWD}/../templates/README.md.template README.md.template
 }
 
+#################################################################################################
+#                                   Application logic                                           #
+#################################################################################################
+
+# Get everything ready
 installJqDependency
 copyTemplate
 
+# Populate variables
 description=$(cat ../../package.json | jq '.description')
 componentFullName=$(cat ../../package.json | jq '.name') #vl-ui-template
 apiName=$(getApiName ${componentFullName})
 demoName=$(getDemoName ${componentFullName})
 
+# Replace all template occurances with correct value
 sed -i "" -e "s/@description@/${description}/g" README.md.template
 sed -i "" -e "s/@fullName@/${componentFullName}/g" README.md.template
 sed -i "" -e "s/@apiName@/${apiName}/g" README.md.template
 sed -i "" -e "s/@demoName@/${demoName}/g" README.md.template
 
+# Clean up after ourselves
+rm -rf README.md
+mv README.md.template README.md
