4185528d5f558054857e6661f289746c0195af82
Sander Kleykens <sander.kleykens@avnu.be>
2020-02-26 10:00:09 +0100

    Verbeter het genereren van de webcomponenten map
    * Haal lijst van bestaande webcomponenten uit github en genereer op basis daarvan de component map
    
    * Voeg optie toe om component map te genereren met of zonder dev dependencies
    
    * Introduceer optie om webcomponenten te excluden

--- generate-component-map.js
+++ generate-component-map.js
@@ -1,17 +1,96 @@
-let fs = require('fs');
-const basePath = process.argv[2];
-let path = require('path');
+const github = require('octonode');
 
-fs.readdirSync(basePath).forEach(file => {
-	if (file.startsWith("webcomponent-vl-")) {
-		const componentPath = path.resolve(basePath, file);
-		const packageJsonInhoud = fs.readFileSync(path.resolve(componentPath, "package.json"));
-		const packageJson = JSON.parse(packageJsonInhoud);
-		const componentName = packageJson.name;
-		Object.keys(packageJson.dependencies).forEach(dependency => {
-			if (dependency.startsWith("vl-") && dependency != "vl-ui-core") {
-				console.log(`"${componentName}" -> "${dependency}"`);
-			}
-		});
-	}
-});
+const githubClient = github.client();
+
+function options() {
+    const options = {
+        dependencies: true,
+        devDependencies: true,
+        exclusions: []
+    };
+    const args = process.argv.slice(2);
+
+    args.forEach(arg => {
+        switch (arg) {
+            case '--no-dependencies':
+            case '-nd':
+                options.dependencies = false;
+                break;
+            case '--no-dev-dependencies':
+            case '-ndd':
+                options.devDependencies = false;
+                break;
+            default:
+                const excludeArgument = '--exclude=';
+                if (arg.startsWith(excludeArgument)) {
+                    options.exclusions = [...options.exclusions, arg.slice(excludeArgument.length)];
+                }
+        }
+    });
+
+    return options;
+}
+
+async function getWebcomponenten() {
+    const org = githubClient.org('milieuinfo');
+    let repos = [], reposInPage = [];
+    let reposPage = 1;
+
+    do {
+        reposInPage = await org.reposAsync({page: reposPage});
+        reposPage++;
+
+        if (reposInPage && reposInPage.length > 0) {
+            repos = [...repos, ...reposInPage[0]];
+        }
+
+    } while (reposInPage && reposInPage.length > 0 && reposInPage[0].length > 0);
+
+    return repos
+        .filter(repo => /^webcomponent-vl-ui/.test(repo.name))
+        .map(repo => githubClient.repo(repo.full_name));
+}
+
+async function packageJsonOfWebcomponent(webcomponent) {
+    const packageJsonContentEntity = await webcomponent.contentsAsync('package.json');
+    if (packageJsonContentEntity && packageJsonContentEntity.length > 0 && packageJsonContentEntity[0].content) {
+        const packageJsonContent = Buffer.from(packageJsonContentEntity[0].content, packageJsonContentEntity[0].encoding).toString('utf-8');
+        return JSON.parse(packageJsonContent);
+    }
+}
+
+function onlyVlDependencies(dependencies) {
+    return Object.keys(dependencies).filter(dependency => dependency.startsWith("vl-") && dependency !== "vl-ui-core");
+}
+
+function dependenciesAsGraph(componentName, dependencies) {
+    return dependencies.map(dependency => `"${componentName}" -> "${dependency}"`);
+}
+
+function printVlDependencies(componentName, dependencies, exclusions) {
+    const filteredDependencies = onlyVlDependencies(dependencies || [])
+        .filter(dependency => !exclusions.includes(dependency));
+    dependenciesAsGraph(componentName, filteredDependencies).forEach(d => console.log(d));
+}
+
+async function printGraph(options) {
+    const webcomponenten = await getWebcomponenten();
+    for (let i = 0; i < webcomponenten.length; i++) {
+        const packageJson = await packageJsonOfWebcomponent(webcomponenten[i]);
+        if (packageJson) {
+            const componentName = packageJson.name;
+
+            if (!options.exclusions.includes(componentName)) {
+                if (options.dependencies) {
+                    printVlDependencies(componentName, packageJson.dependencies, options.exclusions);
+                }
+
+                if (options.devDependencies) {
+                    printVlDependencies(componentName, packageJson.devDependencies, options.exclusions);
+                }
+            }
+        }
+    }
+}
+
+return printGraph(options()).catch(error => console.error(error));
