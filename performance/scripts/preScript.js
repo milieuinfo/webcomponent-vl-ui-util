002c8caa3f500eccc09b23e166bdb5e874243ca1
cambiph <philippe.cambien@gmail.com>
2020-03-20 09:54:00 +0100

    Refactor preScript om leesbaarheid te verhogen

--- performance/scripts/preScript.js
+++ performance/scripts/preScript.js
@@ -6,22 +6,29 @@ module.exports = async function (context, commands) {
     const port = '8080';
     const path = 'demo/performance.html';
 
+    async function openDemoPage() {
+        return commands.navigate(`http://${hostname}:${port}/${path}`);
+    }
+
+    async function countDemoComponents() {
+        await countDemoComponents();
+        return commands.js.run('return document.querySelectorAll(".demo").length > 0');
+    }
+
     return new Promise(resolve => {
         function poll() {
             setTimeout(async () => {
                 http.get(`http://${hostname}:${port}`, async (res) => {
                     const { statusCode } = res;
                     if (statusCode === 200) {
+                        context.log.info(`App available on ${hostname}:${port}.`);
                         try {
-                            context.log.info(`App available on ${hostname}:${port}.`);
-                            await commands.navigate(`http://${hostname}:${port}/${path}`);
-                            const demoComponents = await commands.js.run('return document.querySelectorAll(".demo").length > 0');
-                            if (demoComponents) {
+                            if ((await countDemoComponents())) {
                                 resolve();
                             } else {
                                 throw e;
                             }
-                        } catch (e){
+                        } catch (e) {
                             throw e;
                         }
                     } else {
