636a2ca608d6ddc638936b2204f339ce53ecac28
cambiph <philippe.cambien@gmail.com>
2020-07-10 14:15:45 +0200

    Refactor

--- scripts/checkDependencies.js
+++ scripts/checkDependencies.js
@@ -1,7 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const data = require('../../../package.json');
 
-(async function() {
-  const packageJson = await fs.promises.readFile(path.join(__dirname, '../../package.json'));
-  console.log('Package JSON: ' + packageJson)
-})();
+const dependencies = data.dependencies;
+const devDependencies = data.devDependencies;
+const peerDependencies = data.peerDependencies;
+
+const allDependencies = Object.assign(dependencies, devDependencies, peerDependencies);
+const regex = new RegExp('^\D?\d*\.\d*\.\d*$');
+let errors = 0;
+
+Object.entries(allDependencies).forEach(([dep, version]) => {
+  if (!regex.test(version)) {
+    errors++;
+    console.error(`Ongeldige versienummer voor ${dep}: ${version}`);
+  }
+});
+
+if (errors > 0) {
+  process.exit(1);
+}
