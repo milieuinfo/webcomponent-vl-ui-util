f3e0117faea082dc6cfc938207387eca328584de
cambiph <philippe.cambien@gmail.com>
2020-08-11 11:40:27 +0200

    Fix pad naar result xml

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,7 @@ module.exports = {
     "full-trace": true,
     "reporter": 'xunit',
     "reporter-option": [
-        'output=results.xml'
+        'output=../../results.xml'
     ],
     "spec": ["../../test/**/*.test.js"],
     "timeout": '10000'
