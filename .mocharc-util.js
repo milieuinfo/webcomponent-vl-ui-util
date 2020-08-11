2d12b0b2185cb33e2ec8b0cebd0717ff2c5fd806
cambiph <philippe.cambien@gmail.com>
2020-08-11 13:45:28 +0200

    Fix paden

--- .mocharc-util.js
+++ .mocharc-util.js
@@ -1,6 +1,9 @@
 module.exports = {
     "full-trace": true,
-    "reporter": 'spec',
+    "reporter": 'xunit',
+    "reporter-option": [
+        'output=../../test/results/util-results.xml'
+    ],
     "spec": ["test/**/*.test.js"],
     "timeout": '10000'
 };
--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,7 @@ module.exports = {
     "full-trace": true,
     "reporter": 'xunit',
     "reporter-option": [
-        'output=../../test/results/results.xml'
+        'output=../../test/results/e2e-results.xml'
     ],
     "spec": ["../../test/**/*.test.js"],
     "timeout": '10000'
