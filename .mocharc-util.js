03a2c854244909a013139c5b54b41b3ad58bda31
cambiph <philippe.cambien@gmail.com>
2020-08-13 08:33:10 +0200

    Verwijder intermediate folder results omdat de xunit plugin geen folders aanmaakt

--- .mocharc-util.js
+++ .mocharc-util.js
@@ -2,7 +2,7 @@ module.exports = {
     "full-trace": true,
     "reporter": 'xunit',
     "reporter-option": [
-        'output=../../test/results/util-results.xml'
+        'output=../../test/util-results.xml'
     ],
     "spec": ["test/**/*.test.js"],
     "timeout": '10000'
