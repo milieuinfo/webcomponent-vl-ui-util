bd3c145427ebd4a35baecdb31ebc5db49944290a
cambiph <philippe.cambien@vlaanderen.be>
2020-09-29 10:04:16 +0200

    Als ontwikkelaar wil ik de Test results in bamboo voor webcomponenten kunnen zien (#203)
    * Initial commit
    
    * Voeg npm run script toe
    
    * Voeg path toe als dep en move build naar scripts folder
    
    * Refactor
    
    * Fix regex
    
    * Refactor en toevoegen unit tests
    
    * Refactor en unit testen
    
    * Voeg wct-xunit dep toe
    
    * Verplaats wct-xunit van devDependencies naar dependencies
    Enable wct-xunit plugin in wct.local.conf
    
    * Configure reporter via mocharc
    
    * Geef reporter options door als array cfr https://github.com/mochajs/mocha/pull/4225
    
    * Fix pad naar result xml
    
    * Config finetuning
    
    * Installeer wct-mocha
    
    * Verwijder wct-mocha (testen wilden niet meer runnen) en voeg wct-junit-converter als plugin toe
    
    * Gebruik wct-xunit-reporter
    
    * Fix paden
    
    * Maak gitignore uniform en kopieer naar component
    
    * Bugfix
    
    * Bugfix
    
    * Bugfix
    
    * Bugfix
    
    * Bugfix
    
    * Bugfix
    
    * Bugfix
    
    * Kopieer gitignore en npmignore via templates omdat .gitignore en .npmginore niet in de gereleasde versie zitten
    
    * copyfiles vervangen door cp
    
    * Maak test result folder aan
    
    * Verwijder intermediate folder results omdat de xunit plugin geen folders aanmaakt
    
    * Verwijder intermediate folder results omdat de xunit plugin geen folders aanmaakt
    
    * gitignore update
    
    * Vervang rsync oplossing in docker-compose door gebruik te maken van dockerignore
    
    * bugfix
    
    * Expose test resultaten naar Bamboo
    
    * Expose test resultaten naar Bamboo
    
    * Merge with master
    
    Co-authored-by: cambiph <philippe.cambien@gmail.com>

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,9 @@
 module.exports = {
     "full-trace": true,
-    "reporter": 'spec',
+    "reporter": 'xunit',
+    "reporter-option": [
+        'output=../../test/e2e-results.xml'
+    ],
     "spec": ["../../test/**/*.test.js"],
     "timeout": '10000'
 };
