f7c3590a8a2f082b4600628e2881c5a61a400bf6
cambiph <philippe.cambien@gmail.com>
2020-10-12 13:06:53 +0200

    Gebruik mocha-multi-reporters zodat zowel via CLI als xunit een report gemaakt word

--- .mocharc-util.js
+++ .mocharc-util.js
@@ -1,9 +1,5 @@
 module.exports = {
     "full-trace": true,
-    "reporter": 'xunit',
-    "reporter-option": [
-        'output=../../test/util-results.xml'
-    ],
     "spec": ["test/**/*.test.js"],
     "timeout": '10000'
 };
