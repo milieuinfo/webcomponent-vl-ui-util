b3b59c51f326d8f19bd731154a5a04fc76e88cd5
cambiph <philippe.cambien@gmail.com>
2020-10-14 15:57:36 +0200

    Update config

--- .mocharc-util.js
+++ .mocharc-util.js
@@ -1,9 +1,9 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'mocha-multi-reporters',
-    "reporter-option": {
-        "configFile": 'reporter-config.json'
-    },
+    "reporter-option": [
+        "configFile=reporter-config.json"
+    ],
     "spec": ["test/**/*.test.js"],
     "timeout": '10000'
 };
