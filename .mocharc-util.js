354f719ca3e63a011cb836243db21e06bdb026d1
cambiph <philippe.cambien@gmail.com>
2020-10-14 11:42:30 +0200

    Alle config naar mocharc verhuizen

--- .mocharc-util.js
+++ .mocharc-util.js
@@ -1,5 +1,9 @@
 module.exports = {
     "full-trace": true,
+    "reporter": 'mocha-multi-reporters',
+    "reporter-options": {
+        "configFile": 'reporter-config.json'
+    },
     "spec": ["test/**/*.test.js"],
     "timeout": '10000'
 };
