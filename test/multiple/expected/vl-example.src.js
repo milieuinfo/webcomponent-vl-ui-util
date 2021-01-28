e7b26f0c1d7a0cb7fb3a45b4863d6f488baf71b1
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 16:09:53 +0100

    improved dist imports generator

--- test/multiple/expected/vl-example.src.js
+++ test/multiple/expected/vl-example.src.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import {vlElement, define} from 'vl-ui-core';
 import '@govflanders/vl-ui-util/dist/js/util.js';
-import 'vl-ui-example/lib/external-library.js';
+import '../lib/external-library.js';
 import 'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-polyfill/dist/index.js';
 import 'vl-ui-button';
-import {analytics} from 'vl-ui-example/src/non-vl.js';
-import {VlExampleOther} from 'vl-ui-example/dist/vl-example-other.src.js';
-import 'vl-ui-example/dist/vl-example-other.src.js';
+import {analytics} from '../src/non-vl.js';
+import {VlExampleOther} from '../dist/vl-example-other.src.js';
+import '../dist/vl-example-other.src.js';
 
 export class VlExample extends vlElement(HTMLElement) {
   constructor() {
